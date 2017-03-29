f87a3a67d13b6abbb917190ec2d8bf3272f48b51
kuba565 <kuba565@gmail.com>
2017-03-29 10:09:33 +0200

    28 mar 2017 v.1.5 ENG

--- js/controllers/PeopleController.js
+++ js/controllers/PeopleController.js
@@ -1,13 +1,11 @@
 app.controller('PeopleController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.peopleList = [
-        {name: 'person 1', date: 2017},
-        {name: 'person 2', date: 2016},
-        {name: 'person 3', date: 2015},
-        {name: 'person 4', date: 2014}
-    ];
+    $scope.peopleList = [];
+
+    $scope.groupList = [];
+
     $scope.add = function () {
         if ($scope.addedName && $scope.addedDate) {
-            $scope.peopleList.push({name: $scope.addedName, date: $scope.addedDate});
+            $scope.peopleList.push({name: $scope.addedName, lastName: $scope.addedLastName, date: $scope.addedDate});
         } else if (!$scope.addedName && !$scope.addedDate) {
             alert('Enter a name and a date!');
         } else if (!$scope.addedName) {
@@ -17,6 +15,7 @@ app.controller('PeopleController', ['$scope', '$routeParams', function ($scope, 
         }
         $scope.addedName = "";
         $scope.addedDate = "";
+        $scope.addedLastName = "";
     };
 
     $scope.random = function () {
@@ -24,17 +23,88 @@ app.controller('PeopleController', ['$scope', '$routeParams', function ($scope, 
         $scope.result = $scope.peopleList[losowa];
     };
 
+    var counter = 0;
+
     $scope.generate = function () {
-        var randomNumber1to100 = Math.floor(Math.random() * 100);
-        var randomDate = Math.floor(Math.random() * 67) + 1950;
 
-        var r = confirm("Do you wish to add a randomly generated person?");
-        if (r === true) {
-            $scope.peopleList.push({name: 'person ' + randomNumber1to100, date: randomDate});
-            alert("added: " + '\n' + 'person ' + randomNumber1to100 + '\n' + 'year: ' + randomDate)
-        } else {
-            alert("No person added :(");
-        }
+        var randomDate = Math.floor(Math.random() * 67) + 1920;
+        var randomLastNameNumber = Math.floor(Math.random() * $scope.lastnameList.length);
+        var randomNameNumber = Math.floor(Math.random() * $scope.namesList.length);
 
-    }
+        var firstSurnameLetter = $scope.lastnameList[randomLastNameNumber].substring(0, 1);
+
+        var surnameRemainder = $scope.lastnameList[randomLastNameNumber].substring(1).toLowerCase();
+
+        var randomSurname = firstSurnameLetter + surnameRemainder;
+
+        var randomName = $scope.namesList[randomNameNumber];
+
+
+        counter += 1;
+        $scope.peopleList.push({name: randomName, lastName: randomSurname + ' ' + counter, date: randomDate});
+    };
+
+    $scope.remove = function (index) {
+
+        $scope.peopleList.splice(index, 1)
+
+
+    };
+
+    $scope.clear = function () {
+
+        $scope.peopleList.splice(0);
+
+    };
+
+    $scope.reset = function () {
+        counter = 0;
+    };
+
+    $scope.addToGroup = function (index) {
+
+        var chosenPerson = $scope.peopleList[index];
+
+        var personToBeAddedToGroup = {
+
+            name: chosenPerson.name,
+            lastName: chosenPerson.lastName,
+            date: chosenPerson.date
+        };
+
+        $scope.peopleList.splice(index, 1);
+
+
+        $scope.groupList.push(personToBeAddedToGroup);
+    };
+
+    $scope.lastnameList = [
+
+        'SMITH',
+        'JONES',
+        'BROWN',
+        'JOHNSON',
+        'WILLIAMS',
+        'MILLER',
+        'TAYLOR',
+        'WILSON'
+
+    ];
+
+    $scope.namesList = [
+
+        'Emma',
+        'Noah',
+        'Olivia',
+        'Liam',
+        'Sophia',
+        'Mason',
+        'Ava',
+        'Jacob',
+        'Isabella',
+        'William',
+        'Mia',
+        'Ethan'
+    ];
+
 }]);
